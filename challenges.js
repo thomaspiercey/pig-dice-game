c2dc4ef474c69168fb5563b66165aeb30a4e88f9
thomaspiercey <tom_piercey@hotmail.co.uk>
2020-06-12 01:37:42 +0100

    Update to the game
    Game now plays with a pair of dice instead of a die.

--- challenges.js
+++ challenges.js
@@ -2,10 +2,12 @@
 GAME RULES:
 
 - The game has 2 players, playing in rounds.
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score.
+- In each turn, a player rolls a pair of dice as many times as he whishes. Each result get added to his ROUND score.
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn.
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn.
 - The first player to reach 100 points on GLOBAL score wins the game.
+
+
 
 */
 
@@ -18,18 +20,18 @@ init();
 document.querySelector(".btn-roll").addEventListener("click", function () {
   if (gamePlaying) {
     // 1. Dice - Randon number
-    var dice = Math.floor(Math.random() * 6) + 1;
-    console.log("Dice roll: " + dice);
-    var firstRoll = dice;
-    console.log(firstRoll);
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
     // 2. Display the result
-    var diceDOM = document.querySelector(".dice");
-    diceDOM.style.display = "block";
-    diceDOM.src = "dice-" + dice + ".png";
+    document.getElementById("dice-1").style.display = "block";
+    document.getElementById("dice-2").style.display = "block";
+    document.getElementById("dice-1").src = "dice-" + dice1 + ".png";
+    document.getElementById("dice-2").src = "dice-" + dice2 + ".png";
+
     // 3. Update the round score IF the rolled number was NOT a 1
-    if (dice !== 1) {
+    if (dice1 !== 1 && dice2 !== 1) {
       // Add score
-      roundScore += dice;
+      roundScore += dice1 + dice2;
       document.querySelector(
         "#current-" + activePlayer
       ).textContent = roundScore;
@@ -48,12 +50,24 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
     // 2. Update the user interface
     document.querySelector("#score-" + activePlayer).textContent =
       scores[activePlayer];
+    // Take player input for winning score and overwrite default of 100
+    var input = document.querySelector(".final-score").value;
+    var winningScore;
+    // Undefined, 0 , null, "" are COERCED to false
+    // Anything else is COERCED to true
+    if (input) {
+      winningScore = input;
+    } else {
+      winningScore = 100;
+    }
+
     // 3. Check if a player won the game
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= winningScore) {
       // Update winning player name to 'Winner!'
       document.querySelector("#name-" + activePlayer).textContent = "Winner!";
       // Hide dice
-      document.querySelector(".dice").style.display = "none";
+      document.getElementById("dice-1").style.display = "none";
+      document.getElementById("dice-2").style.display = "none";
       // Update panel to winner styling
       document
         .querySelector(".player-" + activePlayer + "-panel")
@@ -81,7 +95,8 @@ function nextPlayer() {
   // Toogle active class for player
   document.querySelector(".player-0-panel").classList.toggle("active");
   document.querySelector(".player-1-panel").classList.toggle("active");
-  document.querySelector(".dice").style.display = "none";
+  document.getElementById("dice-1").style.display = "none";
+  document.getElementById("dice-2").style.display = "none";
 }
 
 // -- START NEW GAME ===========================================================
@@ -96,7 +111,8 @@ function init() {
   // Set gamePlaying to true on intialise
   gamePlaying = true;
   // Reset on load
-  document.querySelector(".dice").style.display = "none";
+  document.getElementById("dice-1").style.display = "none";
+  document.getElementById("dice-2").style.display = "none";
   document.getElementById("score-0").textContent = "0";
   document.getElementById("score-1").textContent = "0";
   document.getElementById("current-0").textContent = "0";
